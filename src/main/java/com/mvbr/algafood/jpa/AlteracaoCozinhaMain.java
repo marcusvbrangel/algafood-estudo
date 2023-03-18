c2a647e617afdbfeb8fbc4c8dde962b399bcb2b0
Marcus Rangel <marcus.vbrangel@gmail.com>
2023-03-18 19:55:20 -0300

    feat: add update cozinha method

--- src/main/java/com/mvbr/algafood/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/mvbr/algafood/jpa/AlteracaoCozinhaMain.java
@@ -6,7 +6,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
 
     public static void main(String[] args) {
 
@@ -16,17 +16,13 @@ public class InclusaoCozinhaMain {
 
         CadastroCozinha cadastroCozinha = appContext.getBean(CadastroCozinha.class);
 
-        Cozinha cozinha1 = new Cozinha();
-        cozinha1.setNome("Brasileira");
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Tailandesa 4");
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Japonesa");
+        cozinha = cadastroCozinha.salvar(cozinha);
 
-        cozinha1 = cadastroCozinha.salvar(cozinha1);
-        cozinha2 = cadastroCozinha.salvar(cozinha2);
-
-        System.out.printf("%d - %s \n", cozinha1.getId(), cozinha1.getNome());
-        System.out.printf("%d - %s \n", cozinha2.getId(), cozinha2.getNome());
+        System.out.printf("%d - %s \n", cozinha.getId(), cozinha.getNome());
 
     }
 }
