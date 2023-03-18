1cc867b1bba3c6e1b6ffcf2efa7277da7ad7dd01
Marcus Rangel <marcus.vbrangel@gmail.com>
2023-03-18 19:27:09 -0300

    feat: add cozinha method

--- src/main/java/com/mvbr/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/mvbr/algafood/jpa/CadastroCozinha.java
@@ -4,6 +4,7 @@ import com.mvbr.algafood.domain.model.Cozinha;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,6 +20,11 @@ public class CadastroCozinha {
 
     public List<Cozinha> listar() {
         return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+    }
+
+    @Transactional
+    public Cozinha adicionar(Cozinha cozinha) {
+        return manager.merge(cozinha);
     }
 
 }
