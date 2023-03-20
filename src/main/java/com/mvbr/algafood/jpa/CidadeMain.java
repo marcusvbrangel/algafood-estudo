bd203741c8894fe3ad5435b1ec4ff6918eeb79fe
Marcus Rangel <marcus.vbrangel@gmail.com>
2023-03-19 21:24:23 -0300

    feat: create tests for formaPagamento, permissao, estado, cidade

---
+++ src/main/java/com/mvbr/algafood/jpa/CidadeMain.java
@@ -0,0 +1,88 @@
+package com.mvbr.algafood.jpa;
+
+import com.mvbr.algafood.AlgafoodEstudoApplication;
+import com.mvbr.algafood.domain.model.Cidade;
+import com.mvbr.algafood.domain.model.Estado;
+import com.mvbr.algafood.domain.repository.CidadeRepository;
+import com.mvbr.algafood.domain.repository.EstadoRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import java.util.List;
+
+public class CidadeMain {
+
+    public static void main(String[] args) {
+
+        ApplicationContext appContext = new SpringApplicationBuilder(AlgafoodEstudoApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CidadeRepository cidadeRepository = appContext.getBean(CidadeRepository.class);
+
+        System.out.println("\n******************************************************************");
+        System.out.println("Cidade - listar \n");
+
+        List<Cidade> cidades = cidadeRepository.listar();
+
+        for (Cidade cidade : cidades) {
+            System.out.printf("%d - %s - %s \n", cidade.getId(), cidade.getNome(), cidade.getEstado().getNome());
+        }
+
+        System.out.println("\n******************************************************************");
+        System.out.println("Cidade - buscar \n");
+
+        Cidade cidade = cidadeRepository.buscar(1L);
+
+        System.out.printf("%d - %s \n", cidade.getId(), cidade.getNome());
+
+        System.out.println("\n******************************************************************");
+        System.out.println("Cidade - salvar/incluir \n");
+
+        EstadoRepository estadoRepository = appContext.getBean(EstadoRepository.class);
+        Estado estado = estadoRepository.buscar(3L);
+
+        cidade = new Cidade();
+        cidade.setNome("Ouro Preto");
+        cidade.setEstado(estado);
+
+        cidade = cidadeRepository.salvar(cidade);
+
+        System.out.printf("%d - %s - %s \n", cidade.getId(), cidade.getNome(), cidade.getEstado().getNome());
+
+        System.out.println("\n******************************************************************");
+        System.out.println("Cidade - salvar/alterar \n");
+
+        estado = estadoRepository.buscar(1L);
+
+        cidade = new Cidade();
+        cidade.setId(1L);
+        cidade.setNome("Rio de Janeiro alterado");
+        cidade.setEstado(estado);
+
+        cidade = cidadeRepository.salvar(cidade);
+
+        System.out.printf("%d - %s \n", cidade.getId(), cidade.getNome());
+
+        System.out.println("\n******************************************************************");
+        System.out.println("Cidade - excluir \n");
+
+        cidade = new Cidade();
+        cidade.setId(2L);
+
+        cidadeRepository.remover(cidade);
+
+        System.out.println("\n******************************************************************");
+        System.out.println("Cidade - listar (novamente) \n");
+
+        List<Cidade> cidades2 = cidadeRepository.listar();
+
+        for (Cidade cidade2 : cidades2) {
+            System.out.printf("%d - %s - %s \n", cidade2.getId(), cidade2.getNome(), cidade2.getEstado().getNome());
+        }
+
+        System.out.println("\n******************************************************************\n");
+
+    }
+
+}
---
+++ src/main/java/com/mvbr/algafood/jpa/PermissaoMain.java
@@ -0,0 +1,79 @@
+package com.mvbr.algafood.jpa;
+
+import com.mvbr.algafood.AlgafoodEstudoApplication;
+import com.mvbr.algafood.domain.model.Permissao;
+import com.mvbr.algafood.domain.repository.PermissaoRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import java.util.List;
+
+public class PermissaoMain {
+
+    public static void main(String[] args) {
+
+        ApplicationContext appContext = new SpringApplicationBuilder(AlgafoodEstudoApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        PermissaoRepository permissaoRepository = appContext.getBean(PermissaoRepository.class);
+
+        System.out.println("\n******************************************************************");
+        System.out.println("Permissão - listar \n");
+
+        List<Permissao> permissoes = permissaoRepository.listar();
+
+        for (Permissao permissao : permissoes) {
+            System.out.printf("%d - %s \n", permissao.getId(), permissao.getNome());
+        }
+
+        System.out.println("\n******************************************************************");
+        System.out.println("Permissão - buscar \n");
+
+        Permissao permissao = permissaoRepository.buscar(1L);
+
+        System.out.printf("%d - %s \n", permissao.getId(), permissao.getNome());
+
+        System.out.println("\n******************************************************************");
+        System.out.println("Permissão - salvar/incluir \n");
+
+        permissao = new Permissao();
+        permissao.setNome("Imprimir");
+
+        permissao = permissaoRepository.salvar(permissao);
+
+        System.out.printf("%d - %s \n", permissao.getId(), permissao.getNome());
+
+        System.out.println("\n******************************************************************");
+        System.out.println("Permissão - salvar/alterar \n");
+
+        permissao = new Permissao();
+        permissao.setId(1L);
+        permissao.setNome("Incluir alterar");
+
+        permissao = permissaoRepository.salvar(permissao);
+
+        System.out.printf("%d - %s \n", permissao.getId(), permissao.getNome());
+
+        System.out.println("\n******************************************************************");
+        System.out.println("Permissão - excluir \n");
+
+        permissao = new Permissao();
+        permissao.setId(2L);
+
+        permissaoRepository.remover(permissao);
+
+        System.out.println("\n******************************************************************");
+        System.out.println("Permissão - listar (novamente) \n");
+
+        List<Permissao> permissoes2 = permissaoRepository.listar();
+
+        for (Permissao permissao2 : permissoes2) {
+            System.out.printf("%d - %s \n", permissao2.getId(), permissao2.getNome());
+        }
+
+        System.out.println("\n******************************************************************\n");
+
+    }
+
+}
